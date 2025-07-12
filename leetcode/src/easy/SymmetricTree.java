d3b70ec609945a4ac3609ee846eb74bb67a671b5
jaehoonlee <jaelee9212>
2025-07-11 21:06:26 -0400

    Added the BFS method to solve isSymmetric problem

--- leetcode/src/easy/SymmetricTree.java
+++ leetcode/src/easy/SymmetricTree.java
@@ -1,5 +1,8 @@
 package easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
@@ -26,6 +29,33 @@ public class SymmetricTree {
         if(t1.val != t2.val) return false;
 
         return dfs(t1.left, t2.right) && dfs(t1.right,  t2.left);
+    }
+
+    public boolean isSymmetricBST(TreeNode root) {
+        if(root == null) return true;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root.left);
+        q.add(root.right);
+
+        while(!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+            if(t1.val != t2.val) return false;
+
+            q.add(t1.left);
+            q.add(t2.right);
+
+            q.add(t2.left);
+            q.add(t1.right);
+        }
+
+
+        return true;
     }
     private class TreeNode {
         int val;
