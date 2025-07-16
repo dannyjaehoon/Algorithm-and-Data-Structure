37248847c26fd0fd4c42f667a8188236f60a15a9
jaehoonlee <jaelee9212>
2025-07-16 19:43:28 -0400

    Solved two string related problems

--- leetcode/src/medium/LongestCommonPrefix.java
+++ leetcode/src/medium/LongestCommonPrefix.java
@@ -1,4 +1,33 @@
 package medium;
 
 public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        // find the longest common prefix string from the strs array
+        // check each character at the same index across all strings
+        // increment count only if all characters at that index are the same
+
+        // nested for loop
+        // for - int i = 0 to minLength of any string in strs
+        // for - int j = 0 to str.length
+        //       check all chracters in strs at index i are the same across all strings.
+
+        int minLength = Integer.MAX_VALUE;
+
+        for(String s : strs) {
+            minLength = Math.min(minLength, s.length());
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i< minLength; i++) {
+            char c = strs[0].charAt(i);
+            for(int j = 1; j <strs.length; j++) {
+                if(c != strs[j].charAt(i)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
--- leetcode/src/medium/LongestPalindromicSubstring.java
+++ leetcode/src/medium/LongestPalindromicSubstring.java
@@ -1,4 +1,48 @@
 package medium;
 
 public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+
+        // A brute-force way would be to check every possible substring and see if its a palindrome, but would tak O(n3) time and isn't efficient
+
+        // expand around center technique
+        // we expand from the center and check on character to the left and one to the right at each step to see if the substring is a palindrome
+
+        // for each character in the string
+        // i try expanding around it in two ways
+        // once treating it as the center of an odd-length palidrome
+        // and once as the center of an even-length palidrome
+
+        // while expanding, I compare characters on the left and right, and stop when they dont match
+        // at each step, I track the longest palindrome I have found
+
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i<s.length(); i++) {
+            int oddLength = findLengthOfParadrome(s, i, i);
+            int evenLength = findLengthOfParadrome(s, i, i +1);
+            int len = Math.max(oddLength, evenLength);
+
+            if(len > end - start) {
+                start = i - (len - 1) /2;
+                end = i + len / 2;
+            }
+        }
+
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = start; i<=end; i++) {
+        //     sb.append(s.charAt(i));
+        // }
+        return s.substring(start, end +1);
+    }
+
+    private int findLengthOfParadrome(String s, int left, int right) {
+        while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
 }
