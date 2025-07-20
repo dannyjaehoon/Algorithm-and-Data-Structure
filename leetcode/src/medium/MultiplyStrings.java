41be221995df79f03004ea28236ca45d9e87b63a
jaehoonlee <jaelee9212>
2025-07-19 20:01:50 -0400

    solved two string questions

--- leetcode/src/medium/MultiplyStrings.java
+++ leetcode/src/medium/MultiplyStrings.java
@@ -1,4 +1,39 @@
 package medium;
 
 public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+
+        if(num1.equals("0") || num2.equals("0")) return "0";
+
+        int n = num1.length();
+        int m = num2.length();
+        int[] result = new int[n + m];
+
+        for(int i = n - 1; i>=0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for(int j = m - 1;j>=0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                int mul = digit1 * digit2;
+                // Multiply the two digits (e.g., 3 * 6 = 18)
+
+                int sum = mul + result[i + j + 1];
+                // Add the product to the existing value at this position
+                // (in case there's already a carry from a previous step)
+
+                result[i + j + 1] = sum % 10;
+                // Keep only the ones place at the current position (e.g., 18 % 10 = 8)
+
+                result[i + j] += sum / 10;
+                // Carry over the tens place to the previous index (e.g., 18 / 10 = 1)
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int num : result) {
+            if(sb.length() == 0 && num == 0) continue; // 앞자리 0 제거
+            sb.append(num);
+        }
+
+        return sb.toString();
+    }
 }
--- leetcode/src/medium/ReverseWordsinaString.java
+++ leetcode/src/medium/ReverseWordsinaString.java
@@ -1,4 +1,37 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseWordsinaString {
+
+    public String reverseWords(String s) {
+        // 1. remove the leading and trailing spaces first by trim()
+        // 2. for loop through each character of s, use StringBuilder to make each word
+        // 3. when we meet ' ', check the length of the StringBuilder to build a word and store it in the list, and then reset the length to 0
+        // 4. for loop the word list from the end and make the sentence
+        // 5. return the reversed string
+
+        s = s.trim();
+        List<String> wordList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()) {
+            if(sb.length() > 0 && c == ' ') {
+                wordList.add(sb.toString());
+                sb.setLength(0);
+            } else if(c != ' ') {
+                sb.append(c);
+            }
+        }
+        if(sb.length() > 0) {
+            wordList.add(sb.toString());
+            sb.setLength(0);
+        }
+        for(int i = wordList.size() - 1; i >= 0;i--) {
+            if(i != 0) sb.append(wordList.get(i)).append(" ");
+            else sb.append(wordList.get(i));
+        }
+        return sb.toString();
+    }
 }
